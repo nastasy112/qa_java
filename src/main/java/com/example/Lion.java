0bd097257e4cac0d3916faf140859f1a9f62972e
Anastasiia Sytova <Sytova.Anastasiia@yandex.ru>
2022-04-11 16:52:28 +0300

    The Lion class isolated from the Feline

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -16,7 +16,12 @@ public class Lion {
         }
     }
 
-    Feline feline = new Feline();
+    // изоляция класса Lion от класса Feline
+    private Feline feline;
+
+    public Lion(Feline feline){
+        this.feline = feline;
+    }
 
     public int getKittens() {
         return feline.getKittens();
