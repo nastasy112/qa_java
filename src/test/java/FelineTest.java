46e200a2e96f24eb859c8b21c9a6593c93f87592
Anastasiia Sytova <Sytova.Anastasiia@yandex.ru>
2022-04-17 13:29:38 +0300

    Fixed tests for Feline

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -2,8 +2,8 @@ import com.example.Feline;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
@@ -12,19 +12,21 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
 
-    @Mock
+    @Spy
     Feline feline;
 
+    // Готово. Сравнить списки.
     @Test
     public void getMeatReturnCorrectList() throws Exception {
-        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         List<String> expectedListOfMeat = Arrays.asList("Животные", "Птицы", "Рыба");
 
         List<String> aclualListOfMeat = feline.eatMeat();
 
+        Mockito.verify(feline).getFood("Хищник");
         Assert.assertEquals("Method getMeat return incorrect value.",expectedListOfMeat, aclualListOfMeat);
     }
 
+    // Готово. Мок не нужен, тк нет зависимости от др. методов или классов
     @Test
     public void getFamilyReturnsCorrectValue(){
         Feline feline = new Feline();
@@ -35,21 +37,25 @@ public class FelineTest {
         Assert.assertEquals("Method getFamily return incorrect value.", expectedFamily, actualFamily );
     }
 
+    // Готово. Есть зависимость от метода .getKittens(int kittensCount). Нужен мок на метод
     @Test
     public void getKittensReturnsCorrectValue(){
-        int expectedKittens = 10;
-        Mockito.when(feline.getKittens()).thenReturn(expectedKittens);
+        int expectedKittens = 1;
+        Mockito.when(feline.getKittens(1)).thenReturn(1);
 
         int actualKittens = feline.getKittens();
 
         Assert.assertEquals("Method getKittens return incorrect value.", expectedKittens, actualKittens );
     }
 
-    // параметризовать?
+    // Готово. Мок не нужен, тк нет зависимости от др. методов или классов
     @Test
     public void getKittensWithParametersCall() {
-        feline.getKittens(10);
+        Feline feline = new Feline();
+        int expectedKittens = 10;
 
-        Mockito.verify(feline).getKittens(Mockito.anyInt());
+        int actualKittens = feline.getKittens(10);
+
+        Assert.assertEquals("Method getKittens with parameter return incorrect value.", expectedKittens, actualKittens);
     }
 }
